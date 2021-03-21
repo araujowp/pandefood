1e51fe1713f94b66e4d36f34687626f4ea8cf75c
Wagner.Araujo <araujowp@gmail>
2021-03-21 19:33:56 -0300

    conclusao do crud, com paginação e troca de informação entre paginas

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,33 +1,66 @@
-
 import 'package:flutter/material.dart';
 import 'package:pandefood/models/user.dart';
+import 'package:pandefood/provider/users.dart';
+import 'package:pandefood/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
-class UserTile extends StatelessWidget{
-
+class UserTile extends StatelessWidget {
   final User user;
+
   const UserTile(this.user);
 
   @override
   Widget build(BuildContext context) {
     final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
-      ? CircleAvatar(child: Icon(Icons.person))
-        : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl),);
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(
+            backgroundImage: NetworkImage(user.avatarUrl),
+          );
     return ListTile(
       leading: avatar,
       title: Text(user.name),
       subtitle: Text(user.email),
-      trailing: Container(width: 100,
-          child: Row(
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.edit),
-                  color: Colors.blue,
-                  onPressed: (){}, ),
-                IconButton(icon: Icon(Icons.delete),
-                  color: Colors.blue,
-                  onPressed: (){}, ),
-              ]
-          )),
+      trailing: Container(
+          width: 100,
+          child: Row(children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.USER_FORM,
+                  arguments: user,
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              color: Colors.blue,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Exclusão de usuario'),
+                    content: Text('Tem certeza'),
+                    actions: [
+                      FlatButton(
+                        child: Text('Não'),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      ),
+                      FlatButton(
+                        child: Text('Sim'),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      ),
+                    ],
+                  ),
+                ).then((confimed) {
+                  if (confimed) {
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+              },
+            ),
+          ])),
     );
   }
-  
 }
